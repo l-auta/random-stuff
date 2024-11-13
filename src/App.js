e5539228590801fda1e80a38f1d65f0116edab6c
lisa <lisaauta018@gmail.com>
2024-11-13 12:19:16 +0300

    retained my things

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import Header from "./components/header";
-import NavBar from "./components/navBar";
 import Steps from "./components/steps";
 import './App.css'
 
 function App(){
   return(
     <div className="app">
-      <div className="header">
-        <Header />
-      </div>
-      <div className="navbar">
-        <NavBar />
-      </div >
       <div>
           <Steps/>
       </div>
